0dd9adc7711d58b2e3cd3dd47e214dd31bc5d84c
kit-foster <64404127+kit-foster@users.noreply.github.com>
2020-08-19 15:27:35 +1200

    hello mocha test

---
+++ test/hellotest.js
@@ -0,0 +1,9 @@
+const asset = require('assert');
+const { assert } = require('console');
+
+describe('a test to check length of hello', () => {
+
+    it('hello test', () => {
+        assert("hello".length == 5);
+    });
+});
