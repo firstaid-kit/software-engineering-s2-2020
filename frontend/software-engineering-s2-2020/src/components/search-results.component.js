8cecfc684c6f5fb42d2d58dd89efacb65f35fc91
kit-foster <64404127+kit-foster@users.noreply.github.com>
2020-10-08 15:55:32 +1300

    retrieving articles from backend
    gets the articles from backend when search button is clicked
    does not display them - someone else needs to do front end

--- frontend/software-engineering-s2-2020/src/components/search-results.component.js
+++ frontend/software-engineering-s2-2020/src/components/search-results.component.js
@@ -1,56 +1,50 @@
-// import React, { Component } from 'react';
-// import collections from 'C:\Users\kirst\Documents\GitHub\software-engineering-s2-2020\backend\collections\article.json';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-// // import axios from 'axios';
+export default class Search extends Component {
+    constructor(props) {
+        super(props);
 
-// export default class Search extends Component {
-//     constructor(props) {
-//         super(props);
-        
-//         this.getKeys = this.getKeys.bind(this); // returns the list of headings to be displayed on top of table (fetched from the key passed in JSON)
-//         this.getHeader = this.getHeader.bind(this); // contains the logic to return the header
-//         this.getRowsData = this.getRowsData.bind(this); // contains the logic for the body of the table
+        this.state = {
+            articles: []
+        };
+    }
 
-//         getKeys = function() {
-//             return collections.keys(this.props.data[0]);
-//         }
+    getArticles = () => {
+        axios.get('HTTP://localhost:5000/article')
+            .then((response) => {
+                const data = response.data;
+                this.setState({articles: data})
+                console.log('data has been recevied');
+            })
+            .catch(() => {
+                alert('Error retrieving data');
+            });
+    }
 
-//         getHeader = function() {
-//             var keys = this.getKeys();
-//             return keys.map((key, index) => {
-//                 return <th key={key}>{key.toUpperCase()}</th>
-//             })
-//         }
+    displayArticles = (articles) => {
+        if(!articles.length) return null;
 
-//         getRowsData = function() {
-//             var items = this.props.data;
-//             var keys = this.getKeys();
-//             return items.map((row, index)=>{
-//             return <tr key={index}><RenderRow key={index} data={row} keys={keys}/></tr>
-//             })
-//         }
-//     }
+        return articles.map((article, index) => (
+            <div keys={index}>
+                <p>{article.author}</p>
+                <p>{article.title}</p>
+                <p>{article.year}</p>
+                <p>{article.doi}</p>
+            </div>
+        ));
+    };
 
-//     render() {
-//         return (
-//             <div>
-//                 <h3>Displaying your search results</h3>
+    componentDidMount = () => {
+        this.getArticles();
+    };
 
-//                 <table>
-//                     <thead>
-//                         <tr>{this.getHeader()}</tr>
-//                     </thead>
-//                     <tbody>
-//                         {this.getRowsData()}
-//                     </tbody>
-//                 </table>
-//             </div>
-//         );
-//     }
-// }
-
-// const RenderRow = (props) => {
-//     return props.keys.map((key, index)=>{
-//         return <td key={props.data[key]}>{props.data[key]}</td>
-//         })
-// }
+    render() {
+        return (
+            <div>
+                <h3>Displaying your search results</h3>
+                    {this.displayArticles(this.state.articles)}
+            </div>
+        );
+    }
+}
